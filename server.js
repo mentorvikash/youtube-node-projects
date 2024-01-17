adac7faee7dd0fa7c833820c200959f6c6ccdc0c
mentorvikash <mentorvikash@gmail.com>
2024-01-17 23:56:29 +0530

    create todo list app

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
-const port = 4000;
+require('dotenv').config();
 
-app.get("/", (req, res) => {
-    res.send("welcome to nodejs project list")
-})
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set("view engine", "ejs");
+
+const indexRoutes = require("./routes/index")
+app.use("/", indexRoutes)
+
+// app.get("/", (req, res) => {
+//     res.send("welcome to nodejs project list")
+// })
+
 
 app.listen(port, () => {
     console.log("server is running at " + port);
