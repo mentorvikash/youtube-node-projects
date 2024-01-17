9373e2e0f052d16e7b69431b726f881a98e20df7
mentorvikash <mentorvikash@gmail.com>
2024-01-17 23:24:59 +0530

    create a portfolio website with node and react

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const React = require('react');
+const path = require('path');
+const ReactDomServer = require('react-dom/server');
+const App = require('./src/component/App').default;
+
 const app = express();
 const port = 4000;
 
+// ======== To server static files
+// app.use(express.static(path.join(__dirname, 'public')))
+// const portfolioPath = path.join(__dirname, 'public', "index.html")
+// app.get("/", (req, res) => {
+//     res.sendFile(portfolioPath)
+// })
+
+// ======== To server react file
+const jsx = React.createElement(App)
+const reactDom = ReactDomServer.renderToString(jsx)
 app.get("/", (req, res) => {
-    res.send("welcome to nodejs project list")
+    res.send(`
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Portfolio Website</title>
+  </head>
+  <body>
+    <div id="root">${reactDom}</div>
+  </body>
+</html>
+    `)
 })
 
 app.listen(port, () => {
---
+++ src/component/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>We want to serve this react file</div>;
+}
+
+export default App;
