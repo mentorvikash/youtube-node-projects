ce56729cf31fdaa9125c7c3fd35db3b8584a2a21
mentorvikash <mentorvikash@gmail.com>
2024-01-19 22:49:36 +0530

    add ejs file in project

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.set("view engine", "ejs");
+
+const indexRoutes = require('./routes/index');
+app.use("/", indexRoutes)
 
 app.get("/", (req, res) => {
     res.send("welcome to nodejs project list")
