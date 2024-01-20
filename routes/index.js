28b51ce1bdc069f91358af2eade8b842a10860e0
mentorvikash <mentorvikash@gmail.com>
2024-01-20 10:45:47 +0530

    set edit and delete views

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 
-const toDoItems = [
+let toDoItems = [
     { id: 1, task: "buy milk" }
 ]
 
@@ -11,6 +11,42 @@ router.get("/", (_, res) => {
 
 router.get("/new", (req, res) => {
     res.render("new")
+})
+
+router.post("/new", (req, res) => {
+    const body = req.body
+    toDoItems.push({ id: toDoItems.length + 1, task: body.task })
+    res.redirect("/")
+})
+
+router.get("/edit/:id", (req, res) => {
+    const { id } = req.params
+    // const {task} = req.body
+    // toDoItems[id].task = task  
+    // console.log({ task })
+    const todo = toDoItems.filter(item => item.id == id)
+    console.log(todo)
+    res.render("editTodo", { todo: todo[0] })
+})
+
+router.post("/edit/:id", (req, res) => {
+    const { id } = req.params
+    const { task } = req.body
+    const updateIndex = toDoItems.findIndex(item => item.id == id)
+    if (updateIndex != -1) {
+        toDoItems[updateIndex].task = task
+        res.redirect("/")
+    } else {
+        res.status(404).send("todo not found")
+    }
+})
+
+router.post("/delete/:id", (req, res) => {
+    let { id } = req.params
+    let newtodos = toDoItems.filter(item => item.id != id)
+    console.log({ newtodos })
+    toDoItems = newtodos
+    res.redirect("/")
 })
 
 module.exports = router
